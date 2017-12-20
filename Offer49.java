2da733c34d0144084b57db4e0816f5f12e2131eb
LaurenYan <lauren_ywj@163.com>
2017-12-20 10:30:08 +0800

    剑指offer

---
+++ Offer49.java
@@ -0,0 +1,24 @@
+package JZhOffer;
+
+/**
+ * Created by ${ywj} on 2017/11/7.
+ */
+public class Offer49 {
+    public int StrToInt(String str) {
+        if (str.equals("") || str.length() == 0)
+            return 0;
+        char[] a = str.toCharArray();
+        int symbol = 0;
+        if (a[0] == '-')
+            symbol = 1;
+        int sum = 0;
+        for (int i = symbol; i < a.length; i++) {
+            if (a[i] == '+')
+                continue;
+            if (a[i] > '9' || a[i] < '0')
+                return 0;
+            sum = sum * 10 + a[i] - '0';
+        }
+        return symbol == 0 ? sum : sum * (-1);
+    }
+}
